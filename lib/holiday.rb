06d5c21664e1060ec23e68667940b15746ce1ace
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-10 20:22:26 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,10 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  puts "Before: " + holiday_hash[season]
   holiday_hash[season] = holiday_name
+  puts "After: " + holiday_hash[season]
+
   holiday_hash[season][holiday_name] = supply_array
   # remember to return the updated hash
   holiday_hash
